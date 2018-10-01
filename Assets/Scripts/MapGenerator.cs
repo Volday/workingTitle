de4aad893a81c45470962487621bbc113d2bc790
Volday <KBC888@yandex.ru>
2018-10-01 23:04:59 +0800

    Noise Map Generator

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public int mapWidth;
+    public int mapHeigth;
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public bool autoUpdate;
+
+    public void GenerateMap() {
+        float[,] noiseMap = Noise.GenerateNoiseMap(seed, offset, mapWidth, mapHeigth, noiseScale, octaves, persistance, lacunarity);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeigth < 1)
+        {
+            mapHeigth = 1;
+        }
+        if (octaves < 1)
+        {
+            octaves = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+
+    }
+}
